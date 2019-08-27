90c95da052254d8103195d2562ff6ce871b315cc
le22cerqueira <47464819+le22cerqueira@users.noreply.github.com>
2019-08-26 21:53:41 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost"; //servidor (local)
+$database = "odontologico"; //nome do banco, pode alterar
+$username = "root"; // usuário padrão
+$password = ""; // senha de conexão do banco de dados.
+
+// Create connection
+$conexao = mysqli_connect($servername, $username, $password, $database);
+?>
